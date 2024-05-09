611b1b76461f5245b3de3fe2eeaa02800a4e99e9
unknown <snehaptr07@gmail.com>
2024-05-09 17:49:59 +0530

    day21

---
+++ Day_21/EmployeeRequestTrackerdbSolution/EmployeeRequestTrackerdb/EmployeeContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRequestTrackerdb
+{
+    public class EmployeeContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-QL1TE6V\SQLEXPRESS;Integrated Security=true;Initial Catalog=dbEmployeeTrackerCF;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Request>()
+               .HasOne(r => r.RaisedByEmployee)
+               .WithMany(e => e.RequestsRaised)
+               .HasForeignKey(r => r.RequestRaisedBy)
+               .OnDelete(DeleteBehavior.Restrict)
+               .IsRequired();
+
+            modelBuilder.Entity<Request>()
+               .HasOne(r => r.RequestClosedByEmployee)
+               .WithMany(e => e.RequestsClosed)
+               .HasForeignKey(r => r.RequestClosedBy)
+               .OnDelete(DeleteBehavior.Restrict);
+
+
+        }
+    }
+}
