adce7282b6147e21048278600a9bf724079c2641
unknown <snehaptr07@gmail.com>
2024-06-03 17:31:05 +0530

    html

--- Day_27/EmployeeRequestTrackerSolution/EmployeeRequestTrackerApp/obj/Debug/net6.0/EmployeeRequestTrackerApp.AssemblyInfo.cs
+++ Day_27/EmployeeRequestTrackerSolution/EmployeeRequestTrackerApp/obj/Debug/net6.0/EmployeeRequestTrackerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeRequestTrackerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3199b2a5f70c0bb44d06c0a147d703e08ac71f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d40d1293db1da97f69030586ed4301016411f42")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeRequestTrackerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeRequestTrackerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
